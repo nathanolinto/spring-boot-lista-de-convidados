1b29fb142005ef90c47a006964c92aedebe1e9b6
Nathan Olinto <nathanolinto@gmail.com>
2022-09-04 12:54:01 -0300

    adiciona Id como uuid

--- java-spring-festas-backend/src/main/java/br/com/portifolio/festas/entities/Convidado.java
+++ java-spring-festas-backend/src/main/java/br/com/portifolio/festas/entities/Convidado.java
@@ -1,10 +1,23 @@
 package br.com.portifolio.festas.entities;
 
+import java.util.UUID;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "convidado")
+@Table(name = "convidados")
 public class Convidado {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    private UUID id;
+
+    @Column(name = "nome", nullable = false)
+    private String name;
+
 }
