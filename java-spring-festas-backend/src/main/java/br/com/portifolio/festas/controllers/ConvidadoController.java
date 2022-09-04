8b84436f4e713c5991daf26105c4b68bd35eb530
Nathan Olinto <nathanolinto@gmail.com>
2022-09-04 16:17:58 -0300

    remove dependencias não utilizadas

--- java-spring-festas-backend/src/main/java/br/com/portifolio/festas/controllers/ConvidadoController.java
+++ java-spring-festas-backend/src/main/java/br/com/portifolio/festas/controllers/ConvidadoController.java
@@ -1,16 +1,11 @@
 package br.com.portifolio.festas.controllers;
 
-import java.util.Optional;
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.portifolio.festas.entities.Convidado;
-import br.com.portifolio.festas.repositories.ConvidadoRepository;
 import br.com.portifolio.festas.services.ConvidadoService;
 
 @RestController
