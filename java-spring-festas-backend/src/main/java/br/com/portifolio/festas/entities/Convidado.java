a531dc01910854b870e40869d95419ad20f683ef
Nathan Olinto <nathanolinto@gmail.com>
2022-09-04 12:21:32 -0300

    iniciando backend spring boot

---
+++ java-spring-festas-backend/src/main/java/br/com/portifolio/festas/entities/Convidado.java
@@ -0,0 +1,10 @@
+package br.com.portifolio.festas.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "convidado")
+public class Convidado {
+    
+}
---
+++ java-spring-festas-backend/src/test/java/br/com/portifolio/festas/FestasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.portifolio.festas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
